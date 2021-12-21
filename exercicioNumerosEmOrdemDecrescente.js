0001e56a0cdb459dec57f34f22599104b800c639
Thais Bertoldi <thais_brt@hotmail.com>
2021-12-21 15:37:21 -0300

    Exercicio organizar os números em ordem decrescente

---
+++ exercicioNumerosEmOrdemDecrescente.js
@@ -0,0 +1,7 @@
+// Write a function that takes an array of numbers as argument
+// It should return an array with the numbers sorted in descending order
+
+function ordemDecrescente(arrNum) {
+    return arrNum.sort().reverse()
+  }
+  console.log(ordemDecrescente([4,1,3,2]))
